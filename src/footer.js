5d967258dc62a65897782f4e60df8b3fc82ce7c1
Shreyas Bhandari L <119317428+ShreyasBhandariL@users.noreply.github.com>
2025-03-31 12:27:49 +0530

    Update footer.js

--- src/footer.js
+++ src/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const footer = () => {
     return (
         <div className="footer">
-            <h5>Handmade Haven &copy; 2024 | Crafted with ❤️ for learning by <a href="https://shreyasbhandaril.github.io/MyProfile/" style={{color:black;}}>Shreyas Bhandari L</a> </h5>
+            <h5>Handmade Haven &copy; 2024 | Crafted with ❤️ for learning by <a href="https://shreyasbhandaril.github.io/MyProfile/" style={{color:"black"}}>Shreyas Bhandari L</a> </h5>
         </div>
     )
 }
