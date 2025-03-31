3b9688a9915b851e74bbedbb9921fd0ce1e8e082
Shreyas Bhandari L <119317428+ShreyasBhandariL@users.noreply.github.com>
2025-03-31 12:23:23 +0530

    Update footer.js

--- src/footer.js
+++ src/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const footer = () => {
     return (
         <div className="footer">
-            <h1>&copy; 2024 All rights reserved </h1>
+            <h5>Handmade Haven &copy; 2024 | Crafted with ❤️ for learning by <a href="https://shreyasbhandaril.github.io/MyProfile/" style="color:black;">Shreyas Bhandari L</a> </h5>
         </div>
     )
 }
